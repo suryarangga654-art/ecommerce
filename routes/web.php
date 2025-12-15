0f8dde9209db00b4d1600fd33434b9814b8be075
faizal <suryarangga654@gmail.com>
2025-12-15 11:31:21 +0700

    project

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/tentang', function () {
+    return view('tentang');
+});
+Route::get('/sapa/{nama}', function ($nama) {
+  return "Halo, $nama! Selamat datang di Toko Online.";
+});
+Route::get('/kategori/{nama?}', function ($nama = 'Semua') {  
+    return "Menampilkan kategori: $nama";
+});
+Route::get('/produk/{id}', function ($id) {
+    return "Detail produk #$id";
+})->name('produk.detail');
